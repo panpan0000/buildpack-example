f070f57f6521888f9b777be42d4a25851abbf961
Peter Pan <Peter.Pan@daocloud.io>
2022-06-22 22:19:40 -0400

    fix typo

--- jar-code/buildjar.sh
+++ jar-code/buildjar.sh
@@ -1,2 +1,2 @@
 javac Test.java
-[root@peter-jenkins-14 jar-code]# jar -cvmf MANIFEST.MF  Hello.jar Test.class
+jar -cvmf MANIFEST.MF  Hello.jar Test.class
--- step.sh
+++ step.sh
@@ -1,13 +1,23 @@
 pushd jar-code/
-buildjar.sh
+#create a jar file to be tested
+bash buildjar.sh
 popd
 
 pushd my-customizer-builder
 # build my own running image based on java
 docker build  . -t peter-java-run-image --target run -f Dockerfile_run_image
-# build my own builder associate with above new image
-pack builder create my-builder:bionic --config ./builder.toml  --pull-policy if-not-present
+# build my own builder associate with above new image, it will create a new builder image `my-builder:bionic`
+YOUR_BUILDER="my-builder:bionic" # Your builder image
+pack builder create ${YOUR_BUILDER} --config ./builder.toml  --pull-policy if-not-present
 popd
 
 # build a new test-jar as output
-pack build test-jar  --path ./jar-code/ --buildpack ./jar-buildpack/  --pull-policy never --builder my-builder:bionic
+pushd jar-code
+pack config trusted-builders add  ${YOUR_BUILDER}
+pack build test-jar --pull-policy never --builder ${YOUR_BUILDER} -v
+#pack build test-jar  --path ./jar-code/ --buildpack ./jar-buildpack/  --pull-policy never
+popd
+
+echo "==================== Try your final image ==================" 
+# try your final java image
+docker run --rm test-jar
